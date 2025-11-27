7e07896859024ab6bde21f16ac4ea9fcc578de35
Tran Tien Dung <hieunt19.09.04@gmail.com>
2025-11-27 22:46:04 +0700

    Create Fraction.java

---
+++ Contest13/Ex1/Fraction.java
@@ -0,0 +1,33 @@
+package Ex1;
+
+/**
+ *
+ * @author Tran Tien Dung
+ */
+
+public class Fraction {
+    private long numerator;
+    private long denominator;
+
+    public Fraction(long numerator, long denominator){
+        this.numerator = numerator;
+        this.denominator = denominator;
+        simplify();
+    }
+
+    private long gcd(long a, long b){
+        if (b == 0) return a;
+        return gcd(b, a % b);
+    }
+
+    private void simplify(){
+        long scalingFactor = gcd(this.numerator, this.denominator);
+        this.numerator /= scalingFactor;
+        this.denominator /= scalingFactor;
+    }
+
+    @Override
+    public String toString(){
+        return this.numerator + "/" + this.denominator;
+    }
+}
